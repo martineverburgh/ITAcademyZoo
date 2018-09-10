4cb20968d0e70ce11cad130a4bfe0a4e60e074f1
zuvio <Githuberik81>
2018-09-10 15:36:29 +0200

    Personeel class toegevoegd met getters&setters
    mapjes doc en docs samengevoegd

--- src/nl/zoo/personeel/Personeel.java
+++ src/nl/zoo/personeel/Personeel.java
@@ -1,12 +1,26 @@
 package nl.zoo.personeel;
 
 public class Personeel {
+    private String name;
     private String functie;
+    private int employeenumber;
 
     public void setFunctie(String functie) {
         this.functie=functie;
     }
     public String getFunctie() {
         return this.functie;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setEmployeenumber(int employeenumber) {
+        this.employeenumber = employeenumber;
+    }
+    public int getEmployeenumber() {
+        return this.employeenumber;
     }
 }
